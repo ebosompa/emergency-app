e85608b9b549614166b2d5c6339f9d36421bfffe
Gianna Torpey <gianna.torpey@gmail.com>
2021-09-12 02:05:42 -0400

    Added audio recording function

--- EmergencyApp/app/src/main/java/com/example/emergencyapp/MainActivity.java
+++ EmergencyApp/app/src/main/java/com/example/emergencyapp/MainActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -21,9 +28,16 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnSendAlert;
+    private Button btnSendAlert;
     private FusedLocationProviderClient fusedLocationClient;
+    private MediaRecorder recorder;
+    private File audio = null;
+    private boolean callForHelp = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +48,12 @@ public class MainActivity extends AppCompatActivity {
         btnSendAlert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendCurrentLocation();
+                callForHelp = !callForHelp;
+                if (callForHelp) {
+                    sendCurrentLocation();
+                } else{
+                    stopRecording();
+                }
             }
         });
     }
@@ -49,7 +68,9 @@ public class MainActivity extends AppCompatActivity {
                     public void onSuccess(Location location) {
                         Toast.makeText(getApplicationContext(), location.toString(), Toast.LENGTH_LONG).show();
                         btnSendAlert.setBackgroundColor(getResources().getColor(R.color.teal_200));
-                        btnSendAlert.setText("Help called");
+                        btnSendAlert.setText("Stop Recording");
+                        startRecording();
+                        makePostRequest(location);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -57,5 +78,53 @@ public class MainActivity extends AppCompatActivity {
                     public void onFailure(@NonNull @NotNull Exception e) {
                     }
                 });
+    }
+
+    private void startRecording() {
+        File dir = Environment.getExternalStorageDirectory();
+        try {
+            audio = File.createTempFile("aha", ".3gp", dir);
+        } catch (IOException e) {
+            Log.e("ko", e.toString());
+            return;
+        }
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(audio.getAbsolutePath());
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.start();
+
+    }
+
+    private void stopRecording() {
+        btnSendAlert.setBackgroundColor(getResources().getColor(R.color.red));
+        btnSendAlert.setText("Help Me");
+        recorder.stop();
+        recorder.release();
+        addRecordingToMediaLibrary();
+    }
+
+    protected void addRecordingToMediaLibrary() {
+        ContentValues values = new ContentValues(4);
+        long current = System.currentTimeMillis();
+        values.put(MediaStore.Audio.Media.TITLE, "audio" + audio.getName());
+        values.put(MediaStore.Audio.Media.DATE_ADDED, (int) (current / 1000));
+        values.put(MediaStore.Audio.Media.MIME_TYPE, "audio/3gpp");
+        values.put(MediaStore.Audio.Media.DATA, audio.getAbsolutePath());
+        ContentResolver contentResolver = getContentResolver();
+        Uri base = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Uri newUri = contentResolver.insert(base, values);
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, newUri));
+        Toast.makeText(this, "Added File " + newUri, Toast.LENGTH_LONG).show();
+    }
+
+    private void makePostRequest(Location location) {
+        String android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
     }
 }
