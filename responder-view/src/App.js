817d6780bb2ee865588e738d09aa29d0dff1b2a0
Ebo Sompa Dennis <ebo.sompa@gmail.com>
2021-09-12 04:35:24 -0400

    Add login page and emergencies view page

--- responder-view/src/App.js
+++ responder-view/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Route} from "react-router-dom";
+import Login from "./Login";
+import ResponderHome from "./ResponderHome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Login}/>
+      <Route exact path="/home" component={ResponderHome}/>
     </div>
   );
 }
